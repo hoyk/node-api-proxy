551d16503884c7d66e65c94dd5ff221e1eaa953e
HOYK <ykang.ho@gmail.com>
2023-01-14 22:16:01 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# API Proxy Server
+
+Node.js based api proxy server which can be used as API proxy or CORS proxy.
+
+**Install packages**
+
+```
+npm install
+```
+
+<br>
+
+**Start server**
+
+```
+npm run start
+```
+
+<br>
+
+**Usage**
+
+- Replace the original url with proxy url
+- Add 'Target-Endpoint' in header
---
+++ index.js
@@ -0,0 +1,58 @@
+const url = require("url");
+const express = require("express");
+const cors = require("cors");
+const request = require("request");
+const rateLimit = require("express-rate-limit");
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+// Rate Limiting
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1min
+  max: 30,
+});
+app.use(limiter);
+
+app.set("trust proxy", 1);
+
+// Add bodyParser (express.json())
+app.use(express.json({ limit: "200kb" }));
+
+// Enable cors
+app.use(cors());
+
+app.all("*", (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    // CORS preflight
+    res.send();
+  } else {
+    let targetURL = req.header("Target-Endpoint");
+    if (!targetURL) {
+      res.status(500).json({ error: "There is no Target-Endpoint header in the request" });
+      return;
+    }
+
+    console.log(req.body);
+
+    res.status(200).json({ success: "OK" });
+
+    request(
+      {
+        url: targetURL + url.parse(req.url, true).path,
+        method: req.method,
+        json: req.body,
+        headers: {
+          Authorization: req.header("Authorization"),
+        },
+      },
+      (error, response, body) => {
+        if (error) {
+          console.error("error: " + response.statusCode);
+        }
+      }
+    ).pipe(res);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
